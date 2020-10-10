3a14bcb5d9dede1484106626f1c6125e2e491a2f
Meet Patel <pmeet7895@gmail.com>
2020-10-10 18:03:11 +0530

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,78 @@
+(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+    if ($.fn.circleProgress) {
+        $('#circle').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+		$('#circle2').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+        $('#circle3').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+        $('#circle4').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+        $('#circle5').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+		$('#circle6').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+		$('#circle7').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+		$('#circle8').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+		$('#circle9').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+		$('#circle10').circleProgress({
+            size: 180,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#2f89fc',
+            thickness: '16',
+            reverse: true
+        });
+		
+    }
+})(jQuery);
